10d3b82097053280cba8a8263d865e5f8bc8dced
rando <158522817+tj-commits@users.noreply.github.com>
2024-09-11 16:35:04 -0500

    Add more packages

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+require('vanilla-javascript')
+require('vapor-js-npm')
+require('none')()
+
 const findIndex = require('lodash/findIndex');
+const one = require('the-number-one')
+const five = require('five')
+const fourtytwo = require('meaning-of-life')
+const is = require('is-equal-to')
 
-var Two = function(){
-	return findIndex([1, 2, 42], item => item === 42);
-};
+function two() {
+	const threeNumbers = [one, five(), fourtytwo]
+	return findIndex(threeNumbers, item => is(item, fourtytwo));
+}
 
-module.exports = Two;
+module.exports = two;
